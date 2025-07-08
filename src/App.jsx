c9754a52109e797a1cd8a5ec577973daa0574351
TheFounder17 <djambazovmartin14@gmail.com>
2025-07-08 17:48:41 +0300

    yo2

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         </ul>
       </nav>
       <main>
-        <Routes>
+        <Routes >
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
