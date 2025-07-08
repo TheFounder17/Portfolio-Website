7c319bb8ff8f7599d1e7439317ecb341f81aad26
TheFounder17 <djambazovmartin14@gmail.com>
2025-07-08 18:04:27 +0300

    fixed the links

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <nav className="navbar">
         <ul>
           <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
+          <li><Link to="/about">About</ Link></li>
           <li><Link to="/projects">Projects</Link></li>
           <li><Link to="/gallery">Gallery</Link></li>
           <li><Link to="/contact">Contact</Link></li>
